b12cb1bf6aafcd0da6c54aa8f8141649b8c51ce0
Ranjitbera <124592068+Ranjitbera@users.noreply.github.com>
2023-03-19 19:51:13 +0530

    Project done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import CountDown from './component/CountDown';
+
+function App() {
+  return (<>
+    <div className='center'>
+      <h1>Count Down Timer</h1>
+      <CountDown />
+    </div>
+  </>)
+}
+export default App;
